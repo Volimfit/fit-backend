bd91f085bc97139bfc4cf0e9ceb5cc294cd6ef37
Sanya Domashnyi ^_^ <megamanok99@gmail.com>
2024-07-10 13:45:32 +0300

    1234

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
- 
+  app.enableCors({
+    origin: '*', // Здесь можно указать конкретные домены или оставить '*' для разрешения всех доменов
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(PORT, () => console.log(`serv start on port = ${PORT}`));
 }
