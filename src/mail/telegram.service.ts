7b6c085092663852d2f8539b9392bc4ffca597f5
Sanya Domashnyi ^_^ <megamanok99@gmail.com>
2025-02-09 19:40:19 +0300

    fix

--- src/mail/telegram.service.ts
+++ src/mail/telegram.service.ts
@@ -7,7 +7,7 @@ export class TelegramService {
   private chatId: string = process.env.CHAT_ID;; // ID сообщества/группы
 
   constructor() {
-    const token = process.env.BOT_ID; // Токен, который ты получил от BotFather
+    const token = process.env.BOT_ID; 
     this.bot = new TelegramBot(token, { polling: true });
  
   
